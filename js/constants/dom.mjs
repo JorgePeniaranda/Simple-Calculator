bb8c843a3b9ff445f3c39e2af1e2785ba6183955
jorgepeniaranda <peniarandajorge@gmail.com>
2024-07-31 13:45:49 -0300

    refactor: Update DOM constants for calculator inputs

--- js/constants/dom.mjs
+++ js/constants/dom.mjs
@@ -1,3 +1,3 @@
-export const BUTTON_CLASSNAMES = ".calculator__button";
+export const BUTTON_CLASSNAMES = "calculator__button";
 export const LAST_INPUT_ID = "input__last-equation";
 export const CURRENT_INPUT_ID = "input__current-equation";

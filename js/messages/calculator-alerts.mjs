da37ee6b36c7ade8d811c30d533da294a9e4fbac
jorgepeniaranda <peniarandajorge@gmail.com>
2024-08-01 08:35:47 -0300

    feat: Add calculator error messages

---
+++ js/messages/calculator-alerts.mjs
@@ -0,0 +1,8 @@
+export const CALCULATOR_ALERTS_MESSAGES = {
+  CALCULATION_ERROR: 'Calculation Error',
+  ERROR: 'Error',
+  FATAL_ERROR: 'Fatal Error',
+  FATAL_UNKNOWN_ERROR: 'Fatal Unknown Error',
+  INPUT_ERROR: 'Input Error',
+  INTERNAL_ERROR: 'Internal Error'
+};
